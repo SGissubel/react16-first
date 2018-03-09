d30c0778c14580bed06e937480c97cca6fd26f3a
Steven Gissubel <steven.gissubel@gmail.com>
2018-03-09 06:54:26 -0500

    enabled and using css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -74,23 +74,23 @@ class App extends Component {
       style.backgroundColor = 'red';
     }
 
-    const classes = [];
+    const assignedClasses = [];
 
     if (this.state.persons.length <=2) {
-      classes.push('red');
+      assignedClasses.push( classes.red );
     }
     if (this.state.persons.length <=1) {
-      classes.push('bold');
+      assignedClasses.push( classes.bold );
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>React App</h1>
         <button
           style={style}
           onClick={this.togglePersonsHandler}
           >Switch Names</button>
-          <p className={classes.join(' ')}>This is the APp Paragraph</p>
+          <p className={assignedClasses.join(' ')}>This is the APp Paragraph</p>
         {persons}
       </div>
     );
