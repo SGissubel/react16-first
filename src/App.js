785bed66904a5f9a65b7023f37668a509ed48c72
Steven Gissubel <steven.gissubel@gmail.com>
2018-03-08 20:07:08 -0500

    setting initial state - setstate on click event in class-based comp -- passing click to functional comp

--- src/App.js
+++ src/App.js
@@ -3,14 +3,39 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Steven', age: 33 },
+      { name: 'Manuel', age: 44 },
+      { name: 'Buttbutt', age: 67 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Bumbum', age: 65 },
+        { name: 'Manuel', age: 44 },
+        { name: 'Buttbutt', age: 67 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
-        <button>Switch Names</button>
-        <Person name="Steve" age="33"/>
-        <Person name="Bobby" age="33">I got hobbies Yall</Person>
-        <Person name="Buttbutt" age="39"/>
+        <button onClick={this.switchNameHandler}>Switch Names</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler}/>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}>I got hobbies Yall</Person>
         
       </div>
     );
